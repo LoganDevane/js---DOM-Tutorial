9d2b200a67a08b1cba5b785fe7bf66552400a805
Logan <126ldevane@frhsd.com>
2024-11-21 13:43:48 -0500

    vid 7

--- js/sandbox2.js
+++
@@ -1,9 +0,0 @@
-var titles = document.getElementsByClassName('title')
-
-//condole.log(Array. isArray(titles));
-//condole.log(Array. isArray(Array.from(titles)));
-
-
-Array.from(titles).forEach(function(item){
-    console.log(item);
-})
