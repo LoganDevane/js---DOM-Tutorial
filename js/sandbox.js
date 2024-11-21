25ffdfa0a86d62df7f74d06157c5f2f8fd3c431b
Logan <126ldevane@frhsd.com>
2024-11-21 13:59:19 -0500

    Vid 8

--- js/sandbox.js
+++ js/sandbox.js
@@ -1,22 +1,9 @@
-const bookList = document.querySelector('#book-list');
+const booklist = document.querySelector('#book-list')
 
-console.log('book list parent element:', bookList.parentElement);
-console.log('book list parent node:', bookList.parentNode);
+console.log('book-list next sibling is:' . booklist.nextSibling);
+console.log('book-list next element sibling is:' . booklist.nextElementSibling);
 
-console.log('all node children:');
-Array.from(bookList.childNodes).forEach(function(node){
-  console.log(node);
-});
+console.log('book-list previous sibling is' . booklist.previousSibling);
+console.log('book-list previous element sibling is' . booklist.previousElementSibling);
 
-
-console.log('all element children:');
-Array.from(bookList.children).forEach(function(node){
-  console.log(node);
-});
-
-const titles = bookList.querySelectorAll('.name');
-
-console.log('Book titles:');
-Array.from(titles).forEach(function(title){
-  console.log(title.textContent);
-});
+.previousElementSibling.querySelector('p').innerHTML +='<br/> Too Cool For EVeryone Else!'
